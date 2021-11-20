c25434eef8ea9a01c6462c221be72d82713694f1
Colin Gray <colin@cgray.dev>
2021-11-19 18:40:14 -0800

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
 function App() {
   return (
     <div className="app">
-      <header className="font-bold text-3xl select-none" >
-        <div className="p-4 px-12">
-          gmoot
+      <header className="flex justify-between items-center">
+        <div className="font-bold text-3xl select-none p-4 px-12">gmoot</div>
+        <div className="font-bold">staking</div>
+        <div className="px-12">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Connect wallet
+          </button>
         </div>
       </header>
+      <div>
+        <div className="flex justify-center"></div>
+      </div>
     </div>
   );
 }
