bcb2d757b68de92b00312134133c335949b71e87
Colin Gray <colin@cgray.dev>
2021-12-07 21:37:19 -0800

    Rename vars to correct values

--- src/containers/ClaimInterface.tsx
+++ src/containers/ClaimInterface.tsx
@@ -6,18 +6,60 @@ import {
   TOKEN_PROGRAM_ID,
 } from "@solana/spl-token";
 import { programs } from "@metaplex/js";
-import { useNotify, useRewarder } from "../hooks";
+import { useNotify, useRewarder, useProgram, useStakeAccount } from "../hooks";
+import { web3 } from "@project-serum/anchor";
+import { Spinner } from "../components";
 
 export function ClaimInterface() {
   const { connection } = useConnection();
   const rewarder = useRewarder();
   const wallet = useWallet();
+  const stakeAccount = useStakeAccount();
   const notify = useNotify();
-  const [pendingRewards, setPendingRewards] = useState<null | number>(null);
+  const program = useProgram();
+  const [tokenCount, setTokenCount] = useState<null | number>(null);
+  const [loading, setLoading] = useState(false);
 
   const claim = useCallback(async () => {
-    console.log("claim");
-  }, []);
+    try {
+      if (
+        !rewarder?.rewardAuthority ||
+        !wallet.publicKey ||
+        !stakeAccount ||
+        !rewarder ||
+        !program
+      ) {
+        throw new Error("Not finished loading");
+      }
+      setLoading(true);
+      const tokenAccountAddress = await Token.getAssociatedTokenAddress(
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        rewarder.data.rewardMint,
+        wallet.publicKey,
+        false
+      );
+
+      const txId = await program.rpc.claim({
+        accounts: {
+          owner: wallet.publicKey.toBase58(),
+          rewarder: rewarder.address.toBase58(),
+          rewardAuthority: rewarder.rewardAuthority.toBase58(),
+          stakeAccount: stakeAccount.address.toBase58(),
+          rewardMint: rewarder.data.rewardMint.toBase58(),
+          rewardAccount: tokenAccountAddress.toBase58(),
+          tokenProgram: TOKEN_PROGRAM_ID.toBase58(),
+          clock: web3.SYSVAR_CLOCK_PUBKEY.toBase58(),
+        },
+      });
+      console.log(`Transaction Id: ${txId}`, txId);
+      setLoading(false);
+      notify("success", `Claiming Successful. Transaction Id: ${txId}`);
+    } catch (e) {
+      setLoading(false);
+      notify("error", `Error: ${e}`);
+    }
+  }, [program, rewarder, stakeAccount, wallet.publicKey, notify]);
 
   useEffect(() => {
     let didCancel = false;
@@ -44,7 +86,8 @@ export function ClaimInterface() {
       if (!didCancel) {
         if (rewarderAccount !== null) {
           const res = programs.deserialize(rewarderAccount?.data);
-          setPendingRewards(res.amount.toNumber());
+          console.log("reward account info", res.amount.toNumber());
+          setTokenCount(res.amount.toNumber());
           try {
           } catch (e) {
             notify("error", `Error: ${e}`);
@@ -55,21 +98,26 @@ export function ClaimInterface() {
 
     requestRewarder();
 
-    // Return teardown
     return teardown;
   }, [rewarder, wallet, connection, notify]);
-
+  const displayValue = tokenCount ? tokenCount * 0.000000001 : null;
   return (
     <div className="flex justify-between align-center items-center">
-      <div>Your pending rewards: {pendingRewards || 0}</div>
+      <div className="flex flex-col">
+        <div>Your tokens: {displayValue || 0}</div>
+        <div>Your pending rewards: {0}</div>
+      </div>
       <div className="w-24 flex justify-center">
-        <button
-          onClick={claim}
-          className="bg-green-500 hover:bg-green-700 font-bold py-1 px-2 rounded"
-          disabled={rewarder === null}
-        >
-          Claim
-        </button>
+        {loading && <Spinner />}
+        {!loading && (
+          <button
+            onClick={claim}
+            className="bg-green-500 hover:bg-green-700 font-bold py-1 px-2 rounded"
+            disabled={rewarder === null || loading}
+          >
+            Claim
+          </button>
+        )}
       </div>
     </div>
   );
